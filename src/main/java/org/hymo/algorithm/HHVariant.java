f59de600f87233229c20143aa982d9450d54f9a3
jacksonpradolima <jacksonpradolima@gmail.com>
2016-11-01 15:08:22 -0200

    Created enum for variations of HH

---
+++ src/main/java/org/hymo/algorithm/HHVariant.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hymo.algorithm;
+
+/**
+ * Hyper-heuristic application variations
+ * @author Jackson Antonio do Prado Lima <jacksonpradolima at gmail.com>
+ * @version 1.0
+ */
+public enum HHVariant {
+    HHInternalReproduction, 
+    HHExternalReproduction
+}
