c2d9a2671a6fcdc85f668260bf656e272bcbd0cc
Jackson Antonio do Prado Lima <jacksonpradolima@users.noreply.github.com>
2017-10-06 13:15:19 -0300

    Update SPEA2 test

--- src/test/java/org/hymo/algorithm/multiobjective/spea2/SPEA2HHBuilderTest.java
+++ src/test/java/org/hymo/algorithm/multiobjective/spea2/SPEA2HHBuilderTest.java
@@ -53,8 +53,9 @@ public class SPEA2HHBuilderTest {
         selection = new BinaryTournamentSelection<>(new RankingAndCrowdingDistanceComparator<BinarySolution>());
         llhs = new ArrayList<>();
 
-        double crossoverProbability = 0.9;
-        double mutationProbability = 1.0 / problem.getNumberOfBits(0);
+        // When call "low-level heuristic" it must be applied
+        double crossoverProbability = 1.0;
+        double mutationProbability = 1.0;
 
         CrossoverOperator singlePoint = new SinglePointCrossover(crossoverProbability);
 
